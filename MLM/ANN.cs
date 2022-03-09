225150d2935d37b8369e3dd310f7080b92630387
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2022-03-09 14:49:32 +0800

    feat: use MathNet package for fast and effecient matrix operations

--- MLM/ANN.cs
+++ MLM/ANN.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace MLM
 {
@@ -14,55 +14,55 @@ namespace MLM
         [JsonProperty]
         private string Name { get; set; }
         [JsonProperty]
-        List<Matrix> W = new List<Matrix>();
+        List<double[,]> Weight = new List<double[,]>();
+        List<Matrix<double>> W = new List<Matrix<double>>();
         [JsonProperty]
-        List<Matrix> B = new List<Matrix>();
-        List<Matrix> FPZ, FPA;
+        List<double[,]> Bias = new List<double[,]>();
+        List<Matrix<double>> B = new List<Matrix<double>>();
+        List<Matrix<double>> FPZ, FPA;
 
         private ANN() { }
 
         public ANN(int input, int[] innerLayers, int output, string name = "")
         {
             if (innerLayers.Length <= 0)
-            {
                 throw new Exception("There must be at least 1 inner layer of 'n' units");
-            }
             Name = name;
             int i = 0;
-            W.Add(new Matrix(input, innerLayers[i], true));
-            B.Add(new Matrix(1, innerLayers[i++], true));
+            W.Add(Matrix<double>.Build.Random(input, innerLayers[i]));
+            B.Add(Matrix<double>.Build.Random(1, innerLayers[i++]));
             for (; i < innerLayers.Length; i++)
             {
-                W.Add(new Matrix(innerLayers[i - 1], innerLayers[i], true));
-                B.Add(new Matrix(1, innerLayers[i], true));
+                W.Add(Matrix<double>.Build.Random(innerLayers[i - 1], innerLayers[i]));
+                B.Add(Matrix<double>.Build.Random(1, innerLayers[i]));
             }
-            W.Add(new Matrix(innerLayers[--i], output, true));
-            B.Add(new Matrix(1, output, true));
+            W.Add(Matrix<double>.Build.Random(innerLayers[--i], output));
+            B.Add(Matrix<double>.Build.Random(1, output));
         }
 
-        private static Matrix Sigmoid(Matrix A)
+        private static Matrix<double> Sigmoid(Matrix<double> A)
         {
-            return 1 / (1 + (-A).Exp());
+            return 1 / (1 + A.Negate().PointwiseExp());
         }
 
-        private static Matrix DSigmoid(Matrix A)
+        private static Matrix<double> DSigmoid(Matrix<double> A)
         {
-            Matrix sigma_z = Sigmoid(A);
-            Matrix g = sigma_z * (1 - sigma_z);
+            Matrix<double> sigma_z = Sigmoid(A);
+            Matrix<double> g = sigma_z.PointwiseMultiply(1 - sigma_z);
             return g;
         }
 
-        public void FP(Matrix xi)
+        public void FP(Matrix<double> xi)
         {
-            if (xi.Width != W[0].Height)
+            if (xi.ColumnCount != W[0].RowCount)
                 throw new Exception("Input width does not match ANN input height.");
-            FPZ = new List<Matrix>();
-            FPA = new List<Matrix>();
-            Matrix A = xi;
+            FPZ = new List<Matrix<double>>();
+            FPA = new List<Matrix<double>>();
+            Matrix<double> A = xi;
             FPA.Add(xi);
             for (int i = 0; i < W.Count; i++)
             {
-                Matrix Zi = A & W[i];
+                Matrix<double> Zi = A * W[i];
                 if (i < W.Count - 1)
                     FPZ.Add(Zi);
                 A = Sigmoid(Zi + B[i]);
@@ -70,53 +70,53 @@ namespace MLM
             }
         }
 
-        public List<Matrix>[] BP(Matrix Y)
+        public List<Matrix<double>>[] BP(Matrix<double> Y)
         {
             int k = W.Count - 1;
-            List<Matrix> D = new List<Matrix>();
-            Matrix T = FPA[FPA.Count - 1] - Y;
+            List<Matrix<double>> D = new List<Matrix<double>>();
+            Matrix<double> T = FPA[FPA.Count - 1] - Y;
             D.Add(T);
             for (int j = FPZ.Count - 1; j >= 0; j--)
             {
-                T = (T & (W[k--].T)) * DSigmoid(FPZ[j]);
+                T = (T * W[k--].Transpose()).PointwiseMultiply(DSigmoid(FPZ[j]));
                 D.Add(T);
             }
-            List<Matrix> dW = new List<Matrix>();
-            List<Matrix> dB = new List<Matrix>();
+            List<Matrix<double>> dW = new List<Matrix<double>>();
+            List<Matrix<double>> dB = new List<Matrix<double>>();
             for (int i = 0; i < W.Count; i++)
             {
-                dW.Add(FPA[i].T & D[W.Count - 1 - i]);
-                dB.Add(B[i] = D[W.Count - 1 - i]);
+                dW.Add(FPA[i].Transpose() * D[W.Count - 1 - i]);
+                dB.Add(D[W.Count - 1 - i]);
             }
-            return new List<Matrix>[] { dW, dB };
+            return new List<Matrix<double>>[] { dW, dB };
         }
 
-        public void Train(List<Matrix> X, List<Matrix> Y, int iter, double alpha = 0.001, double beta1 = 0.9, double beta2 = 0.999, double epsilon = 10e-8)
+        public void Train(List<Matrix<double>> X, List<Matrix<double>> Y, int iter, double alpha = 0.001, double beta1 = 0.9, double beta2 = 0.999, double epsilon = 10e-8)
         {
             /*
              * This optimizer is based of 'diffGrad' a variant of adam optimizer.
              * https://ieeexplore.ieee.org/document/8939562
              */
-            List<Matrix> m_dW = new List<Matrix>();
-            List<Matrix> m_dB = new List<Matrix>();
-            List<Matrix> v_dW = new List<Matrix>();
-            List<Matrix> v_dB = new List<Matrix>();
-            foreach (Matrix dWi in W)
+            List<Matrix<double>> m_dW = new List<Matrix<double>>();
+            List<Matrix<double>> m_dB = new List<Matrix<double>>();
+            List<Matrix<double>> v_dW = new List<Matrix<double>>();
+            List<Matrix<double>> v_dB = new List<Matrix<double>>();
+            foreach (Matrix<double> dWi in W)
             {
-                m_dW.Add(new Matrix(dWi.Height, dWi.Width));
-                v_dW.Add(new Matrix(dWi.Height, dWi.Width));
+                m_dW.Add(Matrix<double>.Build.Dense(dWi.RowCount, dWi.ColumnCount));
+                v_dW.Add(Matrix<double>.Build.Dense(dWi.RowCount, dWi.ColumnCount));
             }
-            foreach (Matrix dBi in B)
+            foreach (Matrix<double> dBi in B)
             {
-                m_dB.Add(new Matrix(dBi.Height, dBi.Width));
-                v_dB.Add(new Matrix(dBi.Height, dBi.Width));
+                m_dB.Add(Matrix<double>.Build.Dense(dBi.RowCount, dBi.ColumnCount));
+                v_dB.Add(Matrix<double>.Build.Dense(dBi.RowCount, dBi.ColumnCount));
             }
             for (int i = 0; i < iter; i++)
             {
                 FP(X[i % X.Count]);
-                List<Matrix>[] deriv = BP(Y[i % Y.Count]);
-                List<Matrix> dW = deriv[0];
-                List<Matrix> dB = deriv[1];
+                List<Matrix<double>>[] deriv = BP(Y[i % Y.Count]);
+                List<Matrix<double>> dW = deriv[0];
+                List<Matrix<double>> dB = deriv[1];
 
                 for (int j = 0; j < m_dW.Count; j++)
                 {
@@ -126,53 +126,56 @@ namespace MLM
 
                 for (int j = 0; j < v_dW.Count; j++)
                 {
-                    v_dW[j] = beta2 * v_dW[j] + ((1 - beta2) * (dW[j] * dW[j]));
-                    v_dB[j] = beta2 * v_dB[j] + ((1 - beta2) * (dB[j] * dB[j]));
+                    v_dW[j] = beta2 * v_dW[j] + ((1 - beta2) * dW[j].PointwisePower(2));
+                    v_dB[j] = beta2 * v_dB[j] + ((1 - beta2) * dB[j].PointwisePower(2));
                 }
 
-                List<Matrix> m_dW_c = new List<Matrix>();
+                List<Matrix<double>> m_dW_c = new List<Matrix<double>>();
+                List<Matrix<double>> m_dB_c = new List<Matrix<double>>();
                 for (int j = 0; j < m_dW.Count; j++)
+                {
                     m_dW_c.Add(m_dW[j] / (1 - Math.Pow(beta1, i + 1)));
-                List<Matrix> m_dB_c = new List<Matrix>();
-                for (int j = 0; j < m_dB.Count; j++)
                     m_dB_c.Add(m_dB[j] / (1 - Math.Pow(beta1, i + 1)));
+                }
 
-                List<Matrix> v_dW_c = new List<Matrix>();
+                List<Matrix<double>> v_dW_c = new List<Matrix<double>>();
+                List<Matrix<double>> v_dB_c = new List<Matrix<double>>();
                 for (int j = 0; j < v_dW.Count; j++)
+                {
                     v_dW_c.Add(v_dW[j] / (1 - Math.Pow(beta2, i + 1)));
-                List<Matrix> v_dB_c = new List<Matrix>();
-                for (int j = 0; j < v_dW.Count; j++)
                     v_dB_c.Add(v_dB[j] / (1 - Math.Pow(beta2, i + 1)));
+                }
 
-                List<Matrix> xi_dW = new List<Matrix>();
+                List<Matrix<double>> xi_dW = new List<Matrix<double>>();
+                List<Matrix<double>> xi_dB = new List<Matrix<double>>();
                 for (int j = 0; j < W.Count; j++)
-                    xi_dW.Add(Sigmoid((W[j] - dW[j]).Abs()));
-                List<Matrix> xi_dB = new List<Matrix>();
-                for (int j = 0; j < B.Count; j++)
-                    xi_dB.Add(Sigmoid((B[j] - dB[j]).Abs()));
+                {
+                    xi_dW.Add(Sigmoid((W[j] - dW[j]).PointwiseAbs()));
+                    xi_dB.Add(Sigmoid((B[j] - dB[j]).PointwiseAbs()));
+                }
 
                 for (int j = 0; j < W.Count; j++)
-                    W[j] = W[j] - (alpha * xi_dW[j] * m_dW_c[j]) / (v_dW_c[j].Sqrt() + epsilon);
+                    W[j] = W[j] - (alpha * xi_dW[j].PointwiseMultiply(m_dW_c[j])).PointwiseDivide(v_dW_c[j].PointwiseSqrt() + epsilon);
                 for (int j = 0; j < B.Count; j++)
-                    B[j] = B[j] - (alpha * xi_dB[j] * m_dB_c[j]) / (v_dB_c[j].Sqrt() + epsilon);
+                    B[j] = B[j] - (alpha * xi_dB[j].PointwiseMultiply(m_dB_c[j])).PointwiseDivide(v_dB_c[j].PointwiseSqrt() + epsilon);
             }
         }
 
-        public Matrix Infer(Matrix Xi)
+        public Matrix<double> Infer(Matrix<double> Xi)
         {
-            Matrix H = Xi;
+            Matrix<double> H = Xi;
             for (int i = 0; i < W.Count; i++)
-                H = Sigmoid((H & W[i]) + B[i]);
+                H = Sigmoid(H * W[i] + B[i]);
             return H;
         }
 
         public override string ToString()
         {
-            string rep = $"{Name} - [{W[0].Height}";
+            string rep = $"{Name} - [{W[0].RowCount}";
             string content = "";
-            foreach (Matrix Wi in W)
+            foreach (Matrix<double> Wi in W)
             {
-                rep += $",{Wi.Width}";
+                rep += $",{Wi.ColumnCount}";
                 content += $"{Wi}\n";
             }
             rep += "]\n\n" + content;
@@ -181,6 +184,10 @@ namespace MLM
 
         public void Save(string fileName)
         {
+            foreach (Matrix<double> xi in W)
+                Weight.Add(xi.ToArray());
+            foreach (Matrix<double> xi in B)
+                Bias.Add(xi.ToArray());
             string jsonString = JsonConvert.SerializeObject(this, Formatting.Indented);
             File.WriteAllText(fileName, jsonString);
         }
@@ -189,11 +196,11 @@ namespace MLM
         {
             string jsonString = File.ReadAllText(fileName);
             JObject result = JObject.Parse(jsonString);
-            List<Matrix> W = new List<Matrix>();
-            IList<JToken> resultW = result["W"].Children().ToList();
+            List<Matrix<double>> W = new List<Matrix<double>>();
+            IList<JToken> resultW = result["Weight"].Children().ToList();
             LoadDeserializedListMatrix(W, resultW);
-            List<Matrix> B = new List<Matrix>();
-            IList<JToken> resultB = result["B"].Children().ToList();
+            List<Matrix<double>> B = new List<Matrix<double>>();
+            IList<JToken> resultB = result["Bias"].Children().ToList();
             LoadDeserializedListMatrix(B, resultB);
             string name = result["Name"].Value<string>();
             ANN ann = new ANN();
@@ -203,11 +210,11 @@ namespace MLM
             return ann;
         }
 
-        private static void LoadDeserializedListMatrix(List<Matrix> M, IList<JToken> resultM)
+        private static void LoadDeserializedListMatrix(List<Matrix<double>> M, IList<JToken> resultM)
         {
             foreach (JToken result in resultM)
             {
-                Matrix xi = new Matrix(result["M"].Value<JArray>().ToObject<double[,]>(), true);
+                Matrix<double> xi = Matrix<double>.Build.DenseOfArray(result.ToObject<double[,]>());
                 M.Add(xi);
             }
         }
